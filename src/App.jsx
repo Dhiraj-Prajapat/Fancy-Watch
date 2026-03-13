535bb8318414a8e3749ee76283f8862f0b36f706
Dhiraj-Prajapat <dheerajprajapat321@gmail.com>
2026-03-13 23:25:32 +0530

    second commit by rahul

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
 import HeroPage from './components/home/HeroPage.jsx';
 
 const App = () => {
-  const name = "Dhiraj";
-
-  const alert = () => {
-    window.alert("Button Clicked");
-  }
 
   return (
     <>
-      <div className='AppComponent'>
-        <div>App Component Name {name}</div>
-        <button onClick={() => alert()}> Alert </button>
-      </div>
       <HeroPage />
     </>
   )
